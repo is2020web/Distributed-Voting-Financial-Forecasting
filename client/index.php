9afe49ea721f6040975abd793d7712b0706f5200
Info Serv Web <is2020web@abv.bg>
2020-10-24 16:01:14 +0300

    Meta data was added.

--- client/index.php
+++ client/index.php
@@ -1,21 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="bg">
 
 <head>
+<meta charset="UTF-8">
+<meta name="author" content="Тодор Балабанов">
+<meta name="keywords" content="финансово прогнозиране, FOREX, система за гласуване">
+<meta name="description" content="Разпределена система за финансово прогнозиране, чрез гласуване на множество потребители.">
+
 <title>Distributed Voting Financial Forecasting</title>
 </head>
 
 <body>
+<h1>Distributed Voting Financial Forecasting</h1>
+<hr>
 
 Какво е вашето очакване за промяната на курса EUR/USD?
 
+<!-- Това са два бутона. -->
+
 <br>
 
-<button>Увеличение</button> 
+<button id="vote_up">Увеличение</button> 
 
-<button>Намаление</button> 
+<button id="vote_down">Намаление</button> 
+
+<hr>
+&copy; 2020 "Информационно обслужване" АД
 
 </body>
-
 </html>
-
