f46f36dea715d1b2d237761af068b9170fa9f539
Info Serv Web <is2020web@abv.bg>
2020-10-24 13:09:39 +0300

    Simple HTML page was written.

---
+++ client/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<title>Distributed Voting Financial Forecasting</title>
+</head>
+
+<body>
+
+Какво е вашето очакване за промяната на курса EUR/USD?
+
+<br>
+
+<button>Увеличение</button> 
+
+<button>Намаление</button> 
+
+</body>
+
+</html>
+
